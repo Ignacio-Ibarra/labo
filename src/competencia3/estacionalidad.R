959d83f2ffde2ba18e8e912b231c333fe5eb50f8
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2022-10-29 07:27:56 -0300

    estacionalidad

--- src/competencia3/estacionalidad.R
+++ src/competencia3/estacionalidad.R
@@ -1,5 +1,6 @@
 require(data.table)
 require(dplyr)
+require(gridExtra)
 setwd("~/buckets/b1/")
 #Parametros del script
 PARAM  <- list()
@@ -11,4 +12,14 @@ datos <- fread(PARAM$dataset)
 
 estacio <- datos[!foto_mes %in% c(202106,202107), .(cant = .N), by = .(foto_mes,clase_ternaria)]
 
-estacio %>% group_by(foto_mes) %>% mutate(prop=cant/sum(cant))
+estacio <- estacio %>% group_by(foto_mes) %>% mutate(prop=cant/sum(cant))
+
+g1 <- ggplot(estacio, aes(x=foto_mes, y=cant)) %>% 
+  geom_line() %>% 
+  facet_wrap(vars(clase_ternaria), scale = "free_y", nrow=1)
+
+g2 <- ggplot(estacio, aes(x=foto_mes, y=prop)) %>% 
+  geom_line() %>% 
+  facet_wrap(vars(clase_ternaria), scale = "free_y", nrow=1)
+
+grid.arrange(g1, g2)
