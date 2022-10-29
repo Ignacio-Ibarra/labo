be6a6902434d5856f9006c254e2578aac69d3978
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2022-10-29 08:14:44 -0300

    estacionalidad

--- src/competencia3/estacionalidad.R
+++ src/competencia3/estacionalidad.R
@@ -15,6 +15,15 @@ datos <- fread(PARAM$dataset)
 
 estacio <- datos[!foto_mes %in% c(202106,202107), .(cant = .N), by = .(foto_mes,clase_ternaria)]
 
+#creo la carpeta donde va el experimento
+dir.create( paste0( "./exp/", PARAM$experimento, "/"), showWarnings = FALSE )
+setwd(paste0( "./exp/", PARAM$experimento, "/"))
+
+fwrite(estacio, 
+      file = "estacionalidad.csv",
+      logical01= TRUE,
+      sep= ",")
+
 estacio <- estacio %>% group_by(ym(foto_mes)) %>% mutate(prop=cant/sum(cant))
 
 
@@ -30,4 +39,6 @@ g2 <- ggplot(estacio, aes(x=ym(foto_mes), y=prop))+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
   theme_light()
 
+png(filename = "estacionalidad.png")
 grid.arrange(g1, g2)
+dev.off()
