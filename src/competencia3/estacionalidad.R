cf37df2aeb5cf21a2a1452792674b476f4e18edc
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2022-10-29 07:29:32 -0300

    estacionalidad

--- src/competencia3/estacionalidad.R
+++ src/competencia3/estacionalidad.R
@@ -14,12 +14,12 @@ estacio <- datos[!foto_mes %in% c(202106,202107), .(cant = .N), by = .(foto_mes,
 
 estacio <- estacio %>% group_by(foto_mes) %>% mutate(prop=cant/sum(cant))
 
-g1 <- ggplot(estacio, aes(x=foto_mes, y=cant)) %>% 
-  geom_line() %>% 
+g1 <- ggplot(estacio, aes(x=foto_mes, y=cant))+
+  geom_line()+
   facet_wrap(vars(clase_ternaria), scale = "free_y", nrow=1)
 
-g2 <- ggplot(estacio, aes(x=foto_mes, y=prop)) %>% 
-  geom_line() %>% 
+g2 <- ggplot(estacio, aes(x=foto_mes, y=prop))+
+  geom_line()+
   facet_wrap(vars(clase_ternaria), scale = "free_y", nrow=1)
 
 grid.arrange(g1, g2)
